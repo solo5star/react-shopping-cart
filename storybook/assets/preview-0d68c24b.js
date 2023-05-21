b51c52dda6061a2537d6e8b56e2178ad3100ab33
solo5star <solo5star@users.noreply.github.com>
2023-05-21 21:54:25 +0000

    deploy: 2008391d071e9a89c5b54ab4dcf549d099c6da9d

--- storybook/assets/preview-0d68c24b.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-f383baa7.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-88c69734.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-88c69734.js","./iframe-f383baa7.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-7fe832a8.js","./index-8ce4a492.js","./index-d475d2ea.js","./_commonjs-dynamic-modules-302442b1.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-0d68c24b.js.map
